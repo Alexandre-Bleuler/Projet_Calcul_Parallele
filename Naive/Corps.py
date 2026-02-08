810704fc5ec2160a97614df4f927a57be9292847
Florient SAKIYE <florientsakiye@mac.home>
2026-02-08 16:17:06 +0100

    Version Naive

--- Naive/Corps.py
+++ Naive/Corps.py
@@ -1,10 +1,4 @@
 
-import numpy as np
-import sdl2
-import sdl2.ext
-from OpenGL.GL import *
-from OpenGL.GLU import *
-import ctypes
 
 G = 1.560339 * 10**(-13)
 
@@ -18,41 +12,7 @@ class corps:
 
 
     def position_vitesse_update(self, position, vitesse, acceleration, delta_t):
-        self.vitesse = vitesse + acceleration * delta_t
-        self.position = position + delta_t * vitesse + (delta_t**2) * acceleration / 2
+        new_vitesse = vitesse + acceleration * delta_t
+        new_position = position + delta_t * vitesse + (delta_t**2) * acceleration / 2
 
-        return position, vitesse
-
-"""
-    def star_color(self, masse):
-    
-        Génère une couleur pour une étoile en fonction de sa masse.
-        Les étoiles massives sont bleues, les moyennes sont jaunes, les petites sont rouges.
-
-        Parameters:
-        -----------
-        mass : float
-            Masse de l'étoile en masses solaires
-
-        Returns:
-        --------
-        color : tuple
-            Couleur RGB (R, G, B) avec des valeurs entre 0 et 255
-    
-        if masse > 5.0:
-            # Étoiles massives: bleu-blanc
-            return (150, 180, 255)
-        elif masse > 2.0:
-            # Étoiles moyennes-massives: blanc
-            return (255, 255, 255)
-        elif masse > 1.0:
-            # Étoiles comme le Soleil: jaune
-            return (255, 255, 200)
-        else:
-            # Étoiles de faible masse: rouge-orange
-            return (255, 150, 100)
-
-
-"""
-
-
+        return new_position, new_vitesse
--- Naive/naive.py
+++ Naive/naive.py
@@ -4,8 +4,10 @@ import sdl2.ext
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import ctypes
+import visual
+from visual import Visualizer3D
 
-import numpy as np
+
 import NCorps
 from NCorps import Ncorps
 import Corps
@@ -13,50 +15,74 @@ from Corps import corps
 
 
 # Pas de temps Fixé
-delta_t = 0.001 * 3.15576e7
+#delta_t = 0.001 * 3.15576e7
+delta_t = 1
+#delta_t = 1e2
+#delta_t = 1e4
 
 # Transformation de données  text en liste de vecteur
 
 collection = np.loadtxt("etoile")
 print(len(collection))
-# print(collection)
 
 # Recharge de la collection de Corps
 systeme = Ncorps(collection)
 
 N = systeme.shape()
-print(N)
+
+# Fonction pour mettre à jour le système, la fonction retourne les nouvelles positions
+
+def system_update(systeme, dt):
 
 
-# Calcul de l'accélération des corps
-A = np.zeros((N, 3))
+    # Calcul des accélérations
+    A = np.zeros((N, 3))
+    for i in range(N):
+        A[i] = systeme.acceleration(i)
 
-for i in range(N):
-    Acceleration = systeme.acceleration(i)
-    A [i] = Acceleration
+    # Mis à jour de la position et de la vitesse de chaque coprs
 
-print(A)
+    for i in range(N):
+
+        #Extraire l'état actuel
+        masse = systeme.collection_corps[i, 0]
+        position = systeme.collection_corps[i, 1:4]
+        vitesse = systeme.collection_corps[i, 4:7]
+
+        # Création d'un objet temporaire pour utiliser les méthodes
+        star = corps(masse, position.copy(), vitesse.copy(), couleur=(255, 255, 255))
+
+        # Calcul de la nouvelle position et vitesse
+        new_position, new_vitesse = star.position_vitesse_update(star.position, star.vitesse, A[i], dt)
+
+        # Mis à jour du système principal
+        systeme.collection_corps[i, 1:4] = new_position
+        systeme.collection_corps[i, 4:7] = new_vitesse
+
+        #print(new_position)
 
 
-# Mis à jour de la position et de la vitesse de chaque coprs
+    return  systeme.collection_corps[:, 1:4].copy()
 
-for i in range(N):
-    Etoile = systeme.etoile(i)
-    # État de depart
 
-    couleur = systeme.star_color(Etoile[0])  # Couleur de l'étoile
+points = systeme.collection_corps[:, 1:4]
 
-    star = corps(Etoile[0], Etoile[1:4], Etoile[4:7], couleur)
-    print(star)
-    print("Position : ", star.position)
-    print("Vitesse : ", star.vitesse)
 
-    # Mis à jour
-    Acceleration = systeme.acceleration(i)
-    n_star = star.position_vitesse_update(star.position, star.vitesse, Acceleration, delta_t)
+colors = np.array([systeme.star_color(star[0]) for star in systeme.collection_corps])
 
-    # Nouvel État
+luminosities = np.ones(systeme.shape())
 
-    print("Nouvelle Position : ", n_star[0])
-    print("Nouvelle Vitesse : ", n_star[1])
+
+bounds = (
+    (points[:,0].min(), points[:,0].max()),
+    (points[:,1].min(), points[:,1].max()),
+    (points[:,2].min(), points[:,2].max())
+)
+
+visualizer = Visualizer3D(points, colors, luminosities, bounds)
+
+visualizer.run(updater=lambda dt: system_update(systeme, dt), dt=delta_t)
+
+
+
 
