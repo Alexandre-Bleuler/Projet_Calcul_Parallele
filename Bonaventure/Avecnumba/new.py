7453b324831add2aea2e4d86dacc97d3561f4e61
Bonaventure Dohemeto <dohemetobo@gmail.com>
2026-02-16 20:18:51 +0100

    first version of numba

--- Bonaventure/Avecnumba/new.py
+++ Bonaventure/Avecnumba/new.py
@@ -3,6 +3,8 @@ import galaxy_generator
 import visualizer3d_vbo
 import time
 
+import pylab as plt
+import numba
 DT = 0.01
 G = 1.560339e-13
 
@@ -15,7 +17,7 @@ def compute_acce(positions, masses):
         diff = positions - positions[i]        
         dist_sq = np.sum(diff**2, axis =1) 
 
-        mask = dist_sq < 10E-8 
+        mask = dist_sq < 10E-8
         dist_sq[mask]
         dist_sq[i] = 1.0
 
@@ -24,11 +26,40 @@ def compute_acce(positions, masses):
         acc[i] = np.sum(G * masses[:, None] * diff * inv_dist3[:, None], axis=0)
     return acc
 
+
+@numba.njit(parallel=True)
+def compute_acce_numba(positions : np.float64, masses : np.float64)-> np.float64:
+    n : numba.int64 = 0
+    diff = np.zeros(len(masses), 1)
+    acc = np.zeros(len(masses), 3, np.int64)
+    for i in numba.prange(acc.shape[0]):
+        for k in range(len(positions)):
+            diff[k] = positions[k] - positions[i] 
+        dist_sq = 0
+        for l in range(len(diff)):
+            dist_sq += diff[l]**2 
+        mask = dist_sq < 10E-8
+        dist_sq[mask]
+        inv_dist3 = 1.0 / (dist_sq * np.sqrt(dist_sq))
+        inv_dist3[i] = 0.0
+
+        #dist_sq = np.sum(diff**2, axis =1)
+        for j in range(acc.shape[1]):
+            acc[i,j] = 0
+            for m in range(j):
+                acc[i,j] += G * masses[m] * (positions[m]-positions[i]) * inv_dist3
+            for m in range(j,acc.shape[1] ):
+                acc[i,j] += G * masses[m] * (positions[m]-positions[i]) * inv_dist3
+            break
+    return acc
+
+
+
 def update():
     global positions, velocities
 
     start = time.time()
-    acc = compute_acce(positions, masses)
+    acc = compute_acce_numba(positions, masses)
     print("Compute time:", time.time() - start)
 
     positions += velocities*DT + 0.5*acc*DT**2
